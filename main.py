4fe9fcccd41da6b5ff83a47d41de0f8e40d01b28
vapeman <kiv@vivaldi.net>
2022-01-20 17:11:14 +0300

    Work commit

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ from typing import Optional
 
 from fastapi import FastAPI
 
+from components.lidar_providers.rplidar_provider.rpLidarProvider import RpLidarProvider
+
+
+lidar = RpLidarProvider('COM4')
+print('Lidar connection status:', lidar.connect())
+print('Lidar info:', lidar.info)
+print('Lidar health:', lidar.health)
 
 app = FastAPI()
 
@@ -9,6 +16,36 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+@app.get('/lidar/scan')
+def read_scan():
+    return {'data': lidar.scans}
+
+
+@app.get('/lidar/reset')
+def reset_scan():
+    lidar.reset()
+
+
+@app.get('/lidar/info')
+def read_info():
+    return {'data': lidar.info}
+
+
+@app.get('/lidar/health')
+def read_scan():
+    return {'data': lidar.health}
+
+
+@app.get('/lidar/motor/stop')
+def read_scan():
+    lidar.stop_motor()
+
+
+@app.get('/lidar/motor/start')
+def read_scan():
+    return {'data': lidar.start_motor()}
 
 
 @app.get("/items/{item_id}")
